2182f3574d99c6600afb51609f2cad88203b1f21
Aidan-Lalonde-Novales <aidan.lalonde.novales@mths.ca>
2022-02-24 22:22:33 +0000

    added py file

---
+++ perimeter_area.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by Aidan Lalonde-Novales
+# Created February 2022
+# This program calculates the area of a rectangle with a size of 5cm x 3cm
+
+
+def main():
+    # this function calculates the perimeter and area of the rectangle
+    print("A rectangle with the dimensions 5cm x 3cm will have the following:")
+    print("")
+    print("Perimeter of {}cm.".format(2 * (3 + 5)))
+    print("Area of {}cm².".format(3 * 5))
+
+
+if __name__ == "__main__":
+    main()
